96dcf16d4bf41f768931c345e798f0ca3a03b761
Juan Torres <jmmtorres1991@yahoo.com>
2019-05-06 12:45:26 -0500

    Added the first of project files to be made

---
+++ StarWarsProject/StarWarsProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsProject
+//
+//  Created by Juan Torres on 5/6/19.
+//  Copyright © 2019 Juan Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
